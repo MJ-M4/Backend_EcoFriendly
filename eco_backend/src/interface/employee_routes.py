e1e2b5d88f7a1a1b8c6464a4b8d7144b6be5d705
eggsclusive <yona.mattar@gmail.com>
2025-05-18 16:47:32 +0300

    Initial commit

--- eco_backend/src/interface/employee_routes.py
+++ eco_backend/src/interface/employee_routes.py
@@ -3,9 +3,9 @@ from src.datalayer.employee_data import get_all_employees
 from src.errors.general_errors import DataFetchError
 from src.statuses.employee_status import fetch_success
 
-employee_bp = Blueprint('employee', __name__, url_prefix='/api/employees')
+employee_bp = Blueprint('employee', __name__)
 
-@employee_bp.route('/', methods=['GET'])
+@employee_bp.route('/allusers', methods=['GET'])
 def list_employees():
     try:
         employees = get_all_employees()
